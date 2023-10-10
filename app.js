b85d34d0aff1e0eeefbbd97c7d7d97c68569544d
dennishvf <dennisk38@nycstudents.net>
2023-10-10 09:29:44 -0400

    hkjgg

---
+++ app.js
@@ -0,0 +1,41 @@
+let animals=[Elephants,Lemurs,Lizards,Penguins,Salmon]
+const Elephants = {
+Name: "Elephant",
+weight: "13000lbs",
+Fur: "No",
+Diet: "Herbivore",
+Status: "Endangered"
+}
+console.log(Elephants.Name)
+const Lemurs = {
+    Name: "Lemur",
+    weight: "10lbs",
+    Fur: "Yes",
+    Diet: "Omnivore",
+    Status: "Endangered"
+    }
+    console.log(Lemurs.Name)
+const Lizards = {
+    Name: "Lizard",
+    weight: "1lbs",
+    Fur: "No",
+    Diet: "Omnivore",
+    Status: "Abundant"
+    }
+    console.log(Lizards.Name)
+    const Penguins = {
+        Name: "Penguin",
+        weight: "60lbs",
+        Fur: "Yes",
+        Diet: "Carnivore",
+        Status: "Abundant"
+        }
+        console.log(Penguins.Name)
+        const Salmon = {
+            Name: "Salmon",
+            weight: "10lbs",
+            Fur: "No",
+            Diet: "Omnivore",
+            Status: "Abundant"
+            }
+            console.log(Salmon.Name)
